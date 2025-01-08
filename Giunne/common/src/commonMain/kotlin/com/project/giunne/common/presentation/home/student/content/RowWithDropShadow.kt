9d638d0aac6f71500dad0643ecc73afb49eebb88
Joonhoo <joonhoo0123@gmail.com>
2025-01-08 23:19:18 +0900

    feat: 선생님 확인 중인 Box UI 추가

---
+++ Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/home/student/content/RowWithDropShadow.kt
@@ -0,0 +1,35 @@
+package com.project.giunne.common.presentation.home.student.content
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import com.project.giunne.common.ui.theme.GPColor
+import com.project.giunne.common.util.gdp
+
+@Composable
+fun RowWithDropShadow(
+    modifier: Modifier,
+    content: @Composable () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .shadow(
+                shape = RoundedCornerShape(16.gdp),
+                ambientColor = GPColor.TextBlack.copy(alpha = 0.1f),
+                elevation = 4.gdp
+            )
+            .clip(RoundedCornerShape(16.gdp))
+            .background(Color.White)
+            .padding(16.gdp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        content()
+    }
+}
---
+++ Giunne/common/src/commonMain/kotlin/com/project/giunne/common/presentation/home/student/content/TeacherCheckingBox.kt
@@ -0,0 +1,66 @@
+package com.project.giunne.common.presentation.home.student.content
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import com.project.giunne.Res
+import com.project.giunne.common.presentation.common.shape.GPSquircleShape
+import com.project.giunne.common.presentation.common.text.GPAnnotatedString
+import com.project.giunne.common.ui.theme.GPColor
+import com.project.giunne.common.util.gdp
+import com.project.giunne.common.util.gsp
+import com.project.giunne.icon_student_check
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun TeacherCheckingBox(
+    modifier: Modifier,
+    teacherStateTitle: String
+) {
+    RowWithDropShadow(
+        modifier = modifier
+            .padding(16.gdp)
+    ) {
+        GPSquircleShape(
+            modifier = Modifier
+                .size(56.gdp),
+            backgroundColor = GPColor.BackgroundLightGray
+        ) {
+            Icon(
+                modifier = Modifier.size((56 / 3).gdp),
+                tint = GPColor.MainOrangeColor,
+                painter = painterResource(Res.drawable.icon_student_check),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.width(10.gdp))
+
+            GPAnnotatedString(
+                text = buildAnnotatedString {
+                    append("선생님이\n")
+                    withStyle(
+                        style = SpanStyle(
+                            color = GPColor.MainOrangeColor,
+                            fontSize = 18.gsp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append(teacherStateTitle)
+                    }
+                    append("이에요!")
+                },
+            )
+
+            Spacer(modifier = Modifier.width(10.gdp))
+        }
+
+    }
+}
